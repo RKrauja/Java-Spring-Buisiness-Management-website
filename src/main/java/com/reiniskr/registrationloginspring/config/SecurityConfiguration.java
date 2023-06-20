43d2cdf2ce5f01d9f7f56467d1595c104ad8b2a7
reiniskr1 <Reinis.Krauja@gmail.com>
2023-06-20 23:37:57 +0300

    1h 29 min

--- src/main/java/com/reiniskr/registrationloginspring/config/SecurityConfiguration.java
+++ src/main/java/com/reiniskr/registrationloginspring/config/SecurityConfiguration.java
@@ -5,17 +5,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.stereotype.Component;
 
 @Configuration
 @EnableWebSecurity
-class SecurityConfiguration {
+class SecurityConfiguration  {
     @Autowired
     private UserService userService;
+
 
 
     @Bean
@@ -30,13 +34,18 @@ class SecurityConfiguration {
         auth.setPasswordEncoder(passwordEncoder());
         return auth;
     }
-    @Bean
 
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth)throws Exception{
+//        auth.authenticationProvider(authenticationProvider());
+//    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .requestMatchers("/registration").permitAll()
-                .requestMatchers("login").permitAll().and()
+                .requestMatchers("/login").permitAll().and()
                 .logout((logout) ->logout
                                 .invalidateHttpSession(true)
                         .clearAuthentication(true)
